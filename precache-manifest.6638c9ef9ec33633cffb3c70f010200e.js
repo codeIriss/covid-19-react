fb28c3ea71f747803173c0fb5206979f40d85bf1
codeIriss <goal.matters.20@gmail.com>
2020-04-15 05:36:59 +0530

    Updates

---
+++ precache-manifest.6638c9ef9ec33633cffb3c70f010200e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71d656509b74d84d7678440dec110840",
+    "url": "/gocoronavirusgo/index.html"
+  },
+  {
+    "revision": "0c02d7a9a9561b6b0783",
+    "url": "/gocoronavirusgo/static/css/main.bcee6028.chunk.css"
+  },
+  {
+    "revision": "168262430b7ed783f33a",
+    "url": "/gocoronavirusgo/static/js/2.8c8d3c39.chunk.js"
+  },
+  {
+    "revision": "ee4ee1ec0345baf3ccdd39c5f31204f6",
+    "url": "/gocoronavirusgo/static/js/2.8c8d3c39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c02d7a9a9561b6b0783",
+    "url": "/gocoronavirusgo/static/js/main.00f3ed1f.chunk.js"
+  },
+  {
+    "revision": "f071276559a97ecd1163",
+    "url": "/gocoronavirusgo/static/js/runtime-main.56d9899e.js"
+  },
+  {
+    "revision": "7646a23dda83ae7eed47c2e3ebd18c7d",
+    "url": "/gocoronavirusgo/static/media/image.7646a23d.png"
+  }
+]);
